541c91e690fab0f3e2a71f5e4cb09b60240fa7df
Loc Mai <locmai0201@gmail.com>
2023-10-13 14:57:08 +0700

    add mason

--- macos/nvim/lua/plugins.lua
+++ macos/nvim/lua/plugins.lua
@@ -12,6 +12,88 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 return require("lazy").setup({
+    -- {{{ IntelliSense
+    {
+        "https://github.com/VonHeikemen/lsp-zero.nvim",
+        dependencies = {
+            "https://github.com/williamboman/mason.nvim",
+            "https://github.com/williamboman/mason-lspconfig.nvim",
+            "https://github.com/neovim/nvim-lspconfig",
+            "https://github.com/hrsh7th/cmp-nvim-lsp",
+            "https://github.com/hrsh7th/nvim-cmp",
+            "https://github.com/L3MON4D3/LuaSnip",
+        },
+        event = "VeryLazy",
+        config = function()
+            require("mason").setup({
+                PATH = "append", -- Prefer system installed language servers
+            })
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+                    "bashls",
+                    "bufls",
+                    "cssls",
+                    "gopls",
+                    "html",
+                    "lua_ls",
+                    "pyright",
+                    "rust_analyzer",
+                    "terraformls",
+                    "tflint",
+                    "tsserver",
+                    "yamlls",
+                },
+                handlers = {
+                    require("lsp-zero").default_setup,
+                },
+            })
+
+            -- Manually start LSP server after lazy load
+            -- TODO use lazy.nvim LazyFile when available
+            vim.cmd("filetype detect")
+
+            local cmp = require("cmp")
+            local cmp_action = require("lsp-zero").cmp_action()
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"] = cmp.mapping.confirm(),
+                    ["<Tab>"] = cmp_action.luasnip_supertab(),
+                    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+                }),
+            })
+
+            require("lsp-zero").on_attach(function(client, bufnr)
+                local opts = { buffer = bufnr }
+                -- TODO clean up?
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+                vim.keymap.set("n", "gR", vim.lsp.buf.rename, opts)
+                vim.keymap.set({ "n", "x" }, "<Leader>=", vim.lsp.buf.format, opts)
+                vim.keymap.set("n", "cA", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+            end)
+        end,
+    },
+
+    {
+        "https://github.com/windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("nvim-autopairs").setup({
+                check_ts = true,
+            })
+        end,
+    },
+
     -- {{{ Syntax highlighting
     {
         "https://github.com/nvim-treesitter/nvim-treesitter",

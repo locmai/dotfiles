2404d8db171a050ff6cf5db2d03570b54caa4d7a
Loc Mai <locmai0201@gmail.com>
2024-04-01 00:45:58 +0700

    update lsp

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,37 +1,46 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = 'BufWritePre',
-        config = function()
-            require "configs.conform"
-        end,
-    },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require "configs.lspconfig"
-        end,
-    },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
 
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "lua-language-server", "stylua",
-                "prettier"
-            },
-        },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "prettier",
+        "gopls",
+        "rust_analyzer",
+        "ruff-lsp",
+        "helm-ls",
+        "htmx-lsp",
+      },
     },
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim", "lua", "vimdoc",
-                "html", "css"
-            },
-        },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
     },
+  },
 }

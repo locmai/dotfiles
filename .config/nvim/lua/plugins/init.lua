a4db2aedc561508891e6d9b52c0311ff16d7df19
Loc Mai <locmai0201@gmail.com>
2024-04-15 10:50:10 +0700

    just life

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -30,6 +30,28 @@ return {
         "terraform-ls",
       },
     },
+    handlers = {
+      function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {
+          capabilities = capabilities,
+        }
+      end,
+
+      ["lua_ls"] = function()
+        local lspconfig = require "lspconfig"
+        lspconfig.lua_ls.setup {
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              runtime = { version = "Lua 5.1" },
+              diagnostics = {
+                globals = { "vim", "it", "describe", "before_each", "after_each" },
+              },
+            },
+          },
+        }
+      end,
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter",
